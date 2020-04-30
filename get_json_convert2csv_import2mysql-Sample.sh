1d47fc91ec5a7903daecaebd8ac569ea9af6f031
brainstomp <42446297+brainstomp@users.noreply.github.com>
2020-04-30 12:11:24 -0400

    Update get_json_convert2csv_import2mysql-Sample.sh

--- get_json_convert2csv_import2mysql-Sample.sh
+++ get_json_convert2csv_import2mysql-Sample.sh
@@ -7,18 +7,18 @@ _now=$(date +%Y%m%d-%H%M%S)
 #
 #Get the data from the AWOS jSON feed and save it with a name that uses the current date and time
 #
-curl -s http://noc.krpj.skydivecsc.com:1780/current > "/home/brainstomp/csc_awos/data/$_now.json"
+curl -s <JSONURL> > "<HostingPath>/awos/data/$_now.json"
 
 #
 #Convert the data from jSON to CSV and move the jSON file to an archive
 #
 #
-/home/brainstomp/csc_awos/bin/json2csv -k timestamp,wind.direction,wind.speed,wind.gust_speed -i /home/brainstomp/csc_awos/data/$_now.json -p -o /home/brainstomp/csc_awos/data/awos_data
-mv /home/brainstomp/csc_awos/data/*.json /home/brainstomp/csc_awos/records/.
+<HostingPath>/awos/bin/json2csv -k timestamp,wind.direction,wind.speed,wind.gust_speed -i <HostingPath>/awos/data/$_now.json -p -o <HostingPath>/awos/data/awos_data
+mv <HostingPath>/awos/data/*.json <HostingPath>/awos/records/.
 
 #
 #Import the CSV data into MySQL and move the CSV file to an archive
 #
-cd /home/brainstomp/csc_awos/data/
-mysqlimport --user=<AWOSDBUser> --password=<AWOSDBUserPassword> --local --fields-terminated-by=, --verbose --ignore-lines=1 csc_awos awos_data
-mv /home/brainstomp/csc_awos/data/awos_data /home/brainstomp/csc_awos/records/$_now.csv
+cd <HostingPath>/awos/data/
+mysqlimport --user=<AWOSDBUser> --password=<AWOSDBUserPassword> --local --fields-terminated-by=, --verbose --ignore-lines=1 awos awos_data
+mv <HostingPath>/awos/data/awos_data <HostingPath>/awos/records/$_now.csv
